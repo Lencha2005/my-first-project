cd55d73095c2a095d5e875c7c1afb5b44755409a
Lena_Bondar <lencha.2005@gmail.com>
2024-07-26 20:42:47 +0300

    add

---
+++ js/tasks.js
@@ -0,0 +1,47 @@
+// ************
+
+// divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6];
+// function learnJavaScript(numbers, divisor) {
+//     const newArr = [];
+//       for(const num of numbers) {
+//       if(num % divisor === 0){
+//       newArr.push(num);
+      
+//       }
+//     }
+//     return newArr;
+// };
+
+// const learnJavaScript = (numbers, divisor) => numbers.filter(num => num % divisor === 0);
+
+//     console.log(learnJavaScript([1, 2, 3, 4, 5, 6], 2));
+
+    // ************
+
+    // const twiceAsOld = (dadYearsOld, sonYearsOld) => dadYearsOld - (sonYearsOld * 2);
+    // const twiceAsOld = (dadYearsOld, sonYearsOld) => dadYearsOld * 2 - dadYearsOld;
+
+    //   console.log(twiceAsOld(36, 7));
+
+    // ************
+
+    //     const oddCount = n => n % 2 !== 0 ? (n - 1) / 2 : n / 2;
+    //   console.log(oddCount(7));
+
+
+    // ************
+        // const stringToNumber = str => Number(str);
+        // console.log(stringToNumber('999'));
+
+// ************ 
+//? всі перші літери великі
+// String.prototype.toJadenCase = function () {
+//     return this
+//     .split(" ")
+//     .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join(' ')
+//   };
+//   const st = 'How can mirrors be real if our eyes arent real';
+//   console.log(st.toJadenCase);
+
+  // ************ 
